3d7e0bbe3d7efae35fcbfdf70dc7c1d4e7f2cd63
Gil Meir <gilmeir@gmail.com>
2025-01-13 20:45:40 -0600

    add hydro fish plugin (#4)

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -11,15 +11,23 @@
           body = "";
         };
       };
+      plugins = [
+        {
+          name = "hydro";
+          src = pkgs.fishPlugins.hydro.src;
+        }
+      ];
     };
 
-    programs.bash.enable = true;
-    programs.bash.bashrcExtra = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
+    programs.bash = {
+      enable = true;
+      bashrcExtra = ''
+        	if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        	then
+        	  shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        	  exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        	fi
+        	'';
+    };
   };
 }
