a03614f041e8fb9f4e4b31b7f99fa8528a89b626
astrick <astrick.harren.at.work@gmail.com>
2025-01-14 21:05:53 -0600

    feat: add hydro color

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -11,6 +11,12 @@
           body = "";
         };
       };
+      interactiveShellInit = ''
+        set -x hydro_color_pwd "$fish_color_cwd";
+        set -x hydro_color_git "$fish_color_comment";
+        set -x hydro_color_prompt "$fish_color_comment";
+        set -x hydro_color_duration "$fish_color_user";
+      '';
       plugins = [
         {
           name = "hydro";
@@ -22,12 +28,13 @@
     programs.bash = {
       enable = true;
       bashrcExtra = ''
-        	if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-        	then
-        	  shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        	  exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-        	fi
-        	'';
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        fi
+      '';
     };
+
   };
 }
